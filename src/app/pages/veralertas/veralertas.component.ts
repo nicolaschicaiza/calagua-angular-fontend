ab5317dd73981a23e90c5b53b298d8abba9681fb
jnicolaschc <jefryn@unicauca.edu.co>
2022-08-01 08:03:20 -0500

    integración BD con ver alertas terminado

--- src/app/pages/veralertas/veralertas.component.ts
+++ src/app/pages/veralertas/veralertas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Alerta } from '../../models/alerta';
+import { AlertaService } from '../../services/alerta.service';
 
 @Component({
   selector: 'ngx-veralertas',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./veralertas.component.scss']
 })
 export class VeralertasComponent implements OnInit {
-
-  constructor() { }
+  VerAlertas: Alerta [] = [];
+  constructor(private _alertaService: AlertaService) { }
 
   ngOnInit(): void {
+    this.obtenerAlerta();
   }
-
+  obtenerAlerta() {
+    this._alertaService.getAlertas().subscribe(data => {
+      console.log(data);
+      this.VerAlertas = data;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
--- src/app/pages/veralertas/veralertas.module.ts
+++ src/app/pages/veralertas/veralertas.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VeralertasComponent } from './veralertas.component';
-
+import{
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule, NbIconModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbUserModule,
+} from '@nebular/theme';
 
 
 @NgModule({
@@ -9,7 +19,17 @@ import { VeralertasComponent } from './veralertas.component';
     VeralertasComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NbInputModule,
+    NbCardModule,
+    NbButtonModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbDatepickerModule,
+    NbSelectModule,
+    NbIconModule,
   ]
 })
 export class VeralertasModule { }
